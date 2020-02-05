93a8a4add6b717c63c30a84fae503fad9a1e2cc0
ssajroudi <sabrislim010@gmail.com>
2020-02-05 18:51:44 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * print "Programming is like building a multilingual puzzle
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
